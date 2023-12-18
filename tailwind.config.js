76fa7631b732627976001e67add6afa0309777e6
ryushin0 <141554987+ryushin01@users.noreply.github.com>
2023-12-18 13:29:48 +0900

    🎉 프로젝트 초기 세팅

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],  theme: {
+    extend: {},
+    colors: {
+      primary: "#000",
+      secondary: "#fff",
+    }
+  },
+  plugins: [],
+}
+
